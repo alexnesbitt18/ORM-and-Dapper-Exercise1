56bc5996ed6d23ee75bd99abe8865b780f1f3404
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-11-28 14:33:16 -0800

    Updated different portions with bonus

---
+++ IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_and_Dapper_Exercise1
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void DeleteProduct(int id);
+    }
+}
